642646ddf33efb972a26ed2188e51d7d4a8aa49b
wpower12 <willpowe@gmail.com>
2019-12-30 15:30:17 -0500

    working tensorize. fixing optimize.

--- SwarmSim/GCRFModel.py
+++ SwarmSim/GCRFModel.py
@@ -24,16 +24,17 @@ class GCRFModel():
 		self.theta = np.array([alpha, beta])
 
 	def train(self, S, X, Y):
-		X = flatten_shape(np.array(X))
-		Y = flatten_shape(np.array(Y))
+		X      = flatten_shape(np.array(X))
+		Y_flat = flatten_shape(np.array(Y))
+		Y      = tensorize_Y(np.array(Y))
 
 		(N, _) = np.shape(S)
 		S = (S/sum(sum(S))) * N
 		L = np.diag(sum(S)) - S
 		
-		R = self.fit_weak_learners(X, Y)
+		R = self.fit_weak_learners(X, Y_flat)
 		(_,_,D) = np.shape(R)
-		R = tensorize_R(R, (N, self.K, self.T, D))
+		R = tensorize_R(R, (N, D, self.K, self.T))
 
 		cons = ({'type': 'ineq', 'fun' : lambda theta: sum(theta[0:K])},
 				{'type': 'ineq', 'fun' : lambda theta: theta[K]})
@@ -57,7 +58,7 @@ class GCRFModel():
 		return np.zeros((3))
 
 def GCRF_objective( theta, L, R, Y ):
-	(N,K,T) = R.shape
+	(N,D,K,T) = R.shape
 	alpha = theta[0:K]
 	beta = theta[K]
 	epsilon = 1e-6
@@ -72,6 +73,7 @@ def GCRF_objective( theta, L, R, Y ):
 		mu = np.linalg.solve(Q,b)
 		e = Y[:,t] - mu
 		neg_ll = neg_ll - np.dot(np.dot(e.T, Q), e) - 0.5*np.log(np.linalg.det(Q_inv) + epsilon)
+	
 	neg_ll = -neg_ll
 	
 	return neg_ll
@@ -83,15 +85,30 @@ def flatten_shape(X):
 		flat[t*N:t*N+N, :] = X[t, :, :]
 	return flat
 
+def tensorize_Y(Y_old):
+	# Just reshaping Y - it is in shape (T, N, D)
+	# Need it to be (N, D, T)
+	(T, N, D) = np.shape(Y_old)
+	Y = np.zeros((N, D, T))
+	for n in range(N):
+		for d in range(D):
+			for t in range(T):
+				Y[n][d][t] = Y_old[t][n][d]
+	return Y
+
+
 def tensorize_R(R_flat, dim):
 	# R_flat    is shape (K, N*T, D)
-	# Want R to be shape (N, K, T, D)?
-	print(np.shape(R_flat))
-	[N, K, T, D] = dim
-	R = np.zeros((N, K, T, D), dtype=float)
-	for t in range(0,T):
-		R[:,t,] = R_flat[:,t*N:t*N+N]
+	# Want R to be shape (N, D, K, T)?
+	[N, D, K, T] = dim
+	R = np.zeros((N, D, K, T), dtype=float)
+	for d in range(D):
+		for k in range(K):
+			for n in range(N):
+				for t in range(T):
+					R[n][d][k][t] = R_flat[k][n*t][d]
 	return R
+
 
 def threshold(S):
 	(N, _) = np.shape(S)
